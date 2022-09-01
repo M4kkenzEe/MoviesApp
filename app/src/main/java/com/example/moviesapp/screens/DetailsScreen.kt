7d0ae1c9f6bfb3a82a2b67d30d62da35496da035
M4kkenzEe <chernigovtsev2001@mail.ru>
2022-09-01 16:12:55 +0300

    update-51

--- app/src/main/java/com/example/moviesapp/screens/DetailsScreen.kt
+++ app/src/main/java/com/example/moviesapp/screens/DetailsScreen.kt
@@ -10,10 +10,8 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
 import coil.compose.rememberImagePainter
 import com.example.moviesapp.MainViewModel
 import com.example.moviesapp.utils.HtmlText
