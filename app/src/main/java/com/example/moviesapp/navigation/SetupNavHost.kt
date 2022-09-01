e3ac05ec879f035be2e66e807e5c132c84df2f0c
M4kkenzEe <chernigovtsev2001@mail.ru>
2022-09-01 16:09:11 +0300

    update-5

--- app/src/main/java/com/example/moviesapp/navigation/SetupNavHost.kt
+++ app/src/main/java/com/example/moviesapp/navigation/SetupNavHost.kt
@@ -39,7 +39,6 @@ fun SetupNavHost(navController: NavHostController, viewModel: MainViewModel) {
 
         composable(route = Screens.Details.route + "/{Id}") { backStackEntry ->
             DetailsScreen(
-                navController = navController,
                 viewModel = viewModel,
                 itemId = backStackEntry.arguments?.getString("Id") ?: "1"
             )
--- app/src/main/java/com/example/moviesapp/utils/Constants.kt
+++ app/src/main/java/com/example/moviesapp/utils/Constants.kt
@@ -1,5 +1,11 @@
 package com.example.moviesapp.utils
 
+import android.widget.TextView
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.text.HtmlCompat
+
 class Constants {
 
     object Screens {
@@ -8,4 +14,14 @@ class Constants {
         const val DETAILS_SCREEN = "detail_screen"
 
     }
+}
+
+@Composable
+fun HtmlText(html: String, modifier: Modifier) {
+    AndroidView(
+        modifier = modifier,
+        factory = { context -> TextView(context) },
+        update = { it.text = HtmlCompat.fromHtml(html, HtmlCompat.FROM_HTML_MODE_COMPACT) }
+    )
+
 }
